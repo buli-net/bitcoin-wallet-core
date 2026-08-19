9c0f6707c91cfd3c077a0b86d9013c191d1ea109
buli <hvlrcb@gmail.com>
2026-08-19 22:06:08 +0700

    Update BrowserActivity.java

--- wallet/src/wallet/ui/BrowserActivity.java
+++ wallet/src/wallet/ui/BrowserActivity.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.view.FrameLayout;
+import android.widget.FrameLayout;
 import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
